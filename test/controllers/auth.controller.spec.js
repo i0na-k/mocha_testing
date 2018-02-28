bf3d5bd91a0ec662c66a444a70fc1826f33ca7d3
ionakathryn <iona-kathryn.evans@actual-experience.com>
2018-02-28 17:56:16 +0000

    refactored to use beforeeach

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -3,21 +3,27 @@ var authController = require('../../controllers/auth.controller');
 
 describe('AuthController', function(){
 	describe('isAuthorized', function(){
+		beforeEach(function(){
+			console.log('Running before each...');
+			authController.setRoles(['user']);
+		})
 		it('Should return false if not authorized', function(){
-			assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+			assert.equal(false, authController.isAuthorized('admin'));
 		})
 		it('Should return true if authorized', function(){
-			assert.equal(true, authController.isAuthorized(['user','admin'], 'admin'))
+			assert.equal(true, authController.isAuthorized('user'));
 		})
 	})
 })
 
 describe('isAuthorizedAsync', function(){
 	it('Should return false if not authorized', function(done){
-		authController.isAuthorizedAsync(['user',], 'admin',
+		this.timeout(2500);
+		authController.setRoles(['user']);
+		authController.isAuthorizedAsync('admin',
 			function(isAuth){
 				assert.equal(false,isAuth);
 				done();
-			})
+			});
 	})
 })
